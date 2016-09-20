90c8078e19e754d4328888e382bd6ed7ed055463
Lucas Arundell <larundell@small-improvements.com>
2016-09-20 23:12:12 +0200

    feat(plugin) scoped vars for registering plugin

--- si-pricing-calculator/si-pricing-calculator.php
+++ si-pricing-calculator/si-pricing-calculator.php
@@ -8,26 +8,58 @@ Author: Small Improvements Software GmbH
 Author URI: 
 Version: 0.1.0
 */
- 
-define( 'EXT_PATH', plugin_dir_path( __FILE__ ) );
-define( 'EXT_URL', plugin_dir_url( __FILE__ ) );
-define( 'ELEM_URL', EXT_URL . '/' );
+ function loadSiPricingCalculator() {
+	define( 'EXT_PATH', plugin_dir_path( __FILE__ ) );
+	define( 'EXT_URL', plugin_dir_url( __FILE__ ) );
+	define( 'ELEM_URL', EXT_URL . '/includes/' );
 
-add_action( 'cornerstone_register_elements', 'si_pricing_calculator_register_elements' );
-add_filter( 'cornerstone_icon_map', 'si_pricing_calculator_icon_map' );
-add_action( 'wp_enqueue_scripts', 'si_pricing_calculator_enqueue' );
+	/* List Elements Here */
+	$elements = array(
+		'SI_Pricing_Calculator'	=> array(
+			'name'		=> 'si-pricing-calculator',
+			'directory'		=> 'si-pricing-calculator',
+		),
+	);
 	
-function si_pricing_calculator_enqueue() {
-	wp_enqueue_style( 'si-pricing-calculator-style', EXT_URL . 'styles/element.css', array(), '0.1.1' );
-	wp_enqueue_script( 'si-pricing-calculator-script', ELEM_URL . 'scripts/element.js', array(), '0.1.1' );
-}
-/* Register Elements */
-function si_pricing_calculator_register_elements() {
-	cornerstone_register_element( 'SI_Pricing_Calculator', 'si-pricing-calculator', EXT_PATH);
-}
+	/* Register Elements */
+	add_action( 'cornerstone_register_elements', function() use ( $elements ) {
+		foreach ($elements as $key => $value) {
+			$className = $key;
+			$name = $value['name'];
+			$directory = $value['directory'];
 
-/* Mapping of SVG icon so when you're searching in CS Elements, it has a nice graphic. */
-function si_pricing_calculator_icon_map( $icon_map ) {
-	$icon_map['si_pricing_calculator'] = EXT_URL . '/assets/svg/icons.svg';
-	return $icon_map;
-}
+			/* see cornerstone/includes/utility/api.php */
+			cornerstone_register_element( $className, $name, EXT_PATH . 'includes/' . $directory );
+		}
+	});
+	
+	/* Enqueue JS and CSS for Elements */
+	add_action( 'wp_enqueue_scripts', function() use ( $elements ) {
+		foreach ($elements as $key => $value) {
+			$title = $key;
+			$namespace = $value['namespace'];
+			$directory = $value['directory'];
+
+			$cssFilePath = EXT_PATH . 'includes/' . $directory . '/styles/element.css';
+			$cssFileUrl = EXT_URL . 'includes/' . $directory . '/styles/element.css';
+			$jsFilePath = EXT_PATH . 'includes/' . $directory . '/scripts/element.js';
+			$jsFileUrl = EXT_URL . 'includes/' . $directory . '/scripts/element.js';
+			
+			if(file_exists($cssFilePath)) {
+				wp_enqueue_style( $namespace . '-style', $cssFileUrl, array(), '0.1.0' );	
+			}
+			if(file_exists($jsFilePath)) {
+				wp_enqueue_script( $namespace . '-script', $jsFileUrl, array(), '0.1.0' );
+			}
+		}
+	});
+
+	add_filter( 'cornerstone_icon_map', 'si_pricing_calculator_icon_map');
+
+	/* Mapping of SVG icon so when you're searching in CS Elements, it has a nice graphic. */
+	function si_pricing_calculator_icon_map( $icon_map ) {
+		$icon_map['si_pricing_calculator'] = EXT_URL . '/assets/svg/icons.svg';
+		return $icon_map;
+	}
+ }
+ loadSiPricingCalculator();
