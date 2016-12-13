8539fe3340241d92baa9c98a1788501afb3a06f6
Mihai Radulescu <contact@mihai-radulescu.com>
2016-12-13 14:46:07 +0200

    Removed 'pricingPlan' from the calculator

--- si-pricing-calculator/includes/si-pricing-calculator/shortcode.php
+++ si-pricing-calculator/includes/si-pricing-calculator/shortcode.php
@@ -42,21 +42,4 @@ $containerAtts = cs_atts( array(
 			<a class="contact-form-trigger" href="https://www.small-improvements.com/contact">Contact us for renewal</a>
 		</div>	
 	</div>
-	<div class="pricingPlan">
-		<div class="price-box" data-plan="gold">
-			<div class="price-box-plan price-monthly" data-interval="monthly">
-				<div class="price">$6</div>
-				<div class="price-description">per user</div>
-				<div class="price-description">per month </div>
-			</div>
-			<div class="price-box-plan price-yearly" data-interval="yearly">
-				<div class="price">$60</div>
-				<div class="price-description">per user</div>
-				<div class="price-description">per year upfront</div>
-			</div>
-			<p class="info">Pricing in US Dollars<span class="staff-size"></span> <span data-tooltip="Volume discount applied."><i class="icon-info-sign"></i></span><br>
-				<span class="biyearly-info">Special offer: Pay <span class="biyearly-price"></span> for two years upfront.</span>
-			</p>
-		</div>
-	</div>
 </div>
